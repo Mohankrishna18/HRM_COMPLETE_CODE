7b077ef57bbf1ee8d4bf94ab5223fbf373f5da11
sravs246 <sravyavinay246@gmail.com>
2022-06-08 12:24:14 +0530

    Rejected employee

--- HRM_LOCAL/HRM_LOCAL_UI/src/pages/LeaveHistory/LeaveHistoryMain.js
+++ HRM_LOCAL/HRM_LOCAL_UI/src/pages/LeaveHistory/LeaveHistoryMain.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import LeaveEmployeeHistory from './LeaveToApply'
+import LeaveEmployeeHistory from './LeaveToApply';
+import { Row, Col, Card, Container } from 'react-bootstrap';
 const LeaveHistoryMain = () => {
   return (
     <div>
-          <LeaveEmployeeHistory />
-    </div>
+    <div style={{ paddingTop: '20px' }}>
+     <Card responsive className="scroll">
+       <Card.Header>
+         <Card.Body>
+           <Card.Title>History of EmployeesLeaves</Card.Title>
+           <Card.Subtitle className="mb-2 text-muted">
+           Employee Leaves
+           </Card.Subtitle>{" "}
+           <Container>
+             <Row>
+               <Col xs={12}>
+               <LeaveEmployeeHistory />
+               </Col>
+
+             </Row>
+           </Container>
+         </Card.Body>
+       </Card.Header>
+     </Card>
+   </div>
+   </div>
+   
   )
 }
 
