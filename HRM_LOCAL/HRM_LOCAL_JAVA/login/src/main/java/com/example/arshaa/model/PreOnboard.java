ac40ae4dc144e21f323e3b885c78beaee0522d6f
murali-miriyala <muralimiriyala34@gmail.com>
2022-07-08 12:59:21 +0530

    Login service was added

---
+++ HRM_LOCAL/HRM_LOCAL_JAVA/login/src/main/java/com/example/arshaa/model/PreOnboard.java
@@ -0,0 +1,15 @@
+package com.example.arshaa.model;
+
+public class PreOnboard {
+
+	private String userType;
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+}
