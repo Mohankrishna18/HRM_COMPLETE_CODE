eb95a3202e6636fcf4eb240be51ba9edbb9846fe
Bhavani0726 <bhavanigudala1999@gmail.com>
2022-07-18 17:49:59 +0530

    Commit Offer Approval Changes

---
+++ HRM_LOCAL/HRM_LOCAL_JAVA/documentUpload_Service/src/test/java/com/arshaa/documentUpload_Service/DocumentUploadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arshaa.documentUpload_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentUploadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
