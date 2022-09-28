90c6e131b49745bb7abf0d51c08dda56293c1a7e
Nikhil Mudhiraj <nikhilmudheraj143@gmail.com>
2022-09-28 18:01:53 +0530

    Hiring Tab Updated

--- HRM_LOCAL/HRM_LOCAL_UI/src/pages/HrDashboard/HrDashboardComponents/HiringsTab.js
+++ HRM_LOCAL/HRM_LOCAL_UI/src/pages/HrDashboard/HrDashboardComponents/HiringsTab.js
@@ -17,6 +17,8 @@ import axios from "../../../Uri"
 function HiringsTab(props) {
 
     const [data, setData] = useState([]);
+    const [month, setMonth] = useState([]);
+    const [today, setToday] = useState([]);
     const [onboardID, setOnboardID] = useState({});
     const [update, setUpdate] = useState(false);
     const [reject, setReject] = useState(false);
@@ -24,6 +26,7 @@ function HiringsTab(props) {
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
     useEffect(() => {
         loadData();
       }, [update, onboardID, reject]);
@@ -38,36 +41,47 @@ function HiringsTab(props) {
   console.log(data.length)
 
 
-//   useEffect(() => {
-//     axios
-//         .get(`/emp/getDetailsforPMOApprovalByOnboardingStatus${onboardingStatus}`)
-//         .then((res) => {
-//             setData(res.data.data);
-//             console.log(res.data);
-//         });
-// }, []);
+  useEffect(() => {
+    axios
+        .get(`emp/getData`)
+        .then((res) => {
+            setMonth(res.data);
+            console.log(res.data);
+        });
+}, []);
+
+useEffect(() => {
+    axios
+        .get(`emp/getDataByDATE`)
+        .then((res) => {
+            setToday(res.data);
+            console.log(res.data);
+        });
+}, []);
+console.log(data)
 
     return (
         <div>
             <Box sx={{ width: '100%', typography: 'body1' }}>
-                <Row  style={{paddingBottom:"10px"}}>
-                    <Col md="2">
-                        <Card style={{padding:"15px",textAlign:"center"}}><h5>Pre Hire</h5><h5>{data.length}</h5></Card>
+                {/* <Row  style={{paddingBottom:"10px"}}>
+                    <Col md="3">
+                        <Card style={{padding:"15px",textAlign:"center"}}><h5>Offer Released</h5><h5>{data.2}</h5></Card>
                     </Col>
-                    <Col md="2">
-                        <Card style={{padding:"15px",textAlign:"center"}}><h5>Onboardings For Today</h5></Card>
+                    <Col md="3">
+                        <Card style={{padding:"15px",textAlign:"center"}}><h5>Onboardings For Today</h5><h5>{today.length}</h5></Card>
                     </Col>
-                    <Col md="2">
-                        <Card style={{padding:"15px",textAlign:"center"}}><h5>Onboardings For This Month</h5></Card>
+                    <Col md="3">
+                        <Card style={{padding:"15px",textAlign:"center"}}><h5>Onboardings For This Month</h5><h5>{month.length}</h5></Card>
                     </Col>
-                </Row>
+                </Row> */}
                 
                 <TabContext value={value} style={{paddingTop:"10px"}}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={handleChange} aria-label="lab API tabs example"style={{backgroundColor:"#ebe8e6",borderRadius:"10px"}} >
-                            <Tab label="Pre Hire" value="1" icon={<FcAcceptDatabase style={{ fontSize: "25px" }} />} />
-                            <Tab label="Onboardings for Today" value="2" icon={<FcAssistant style={{ fontSize: "25px" }} />} />
-                            <Tab label="Onboardings for This Month" value="3" icon={<FcFlowChart style={{ fontSize: "25px" }} />} />
+                    <Box sx={{ borderBottom: 0, borderColor: 'divider' }}>
+                        <TabList onChange={handleChange} aria-label="lab API tabs example"style={{backgroundColor:"#ebe8e6",borderRadius:"5px"}} >
+                           
+                            <Tab label="Offer Released" value="1"  style={{paddingRight:"2%",paddingLeft:"2%"}} icon={<FcAcceptDatabase style={{ fontSize: "25px" }} />}></Tab>
+                            <Tab label="Onboardings for Today" value="2" style={{paddingRight:"2%",paddingLeft:"2%"}} icon={<FcAssistant style={{ fontSize: "25px" }} />} />
+                            <Tab label="Onboardings for This Month" value="3" style={{paddingRight:"2%",paddingLeft:"2%"}} icon={<FcFlowChart style={{ fontSize: "25px" }} />} />
                         </TabList>
                     </Box>
                     <TabPanel value="1"> <PreHire/></TabPanel>
