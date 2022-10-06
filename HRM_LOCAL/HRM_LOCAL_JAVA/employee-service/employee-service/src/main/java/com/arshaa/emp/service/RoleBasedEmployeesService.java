1be581df6ce2825f3b68bfe0dfa78586f9dc861c
devi1897 <94985408+devi1897@users.noreply.github.com>
2022-10-06 17:24:30 +0530

    empModification

--- HRM_LOCAL/HRM_LOCAL_JAVA/employee-service/employee-service/src/main/java/com/arshaa/emp/service/RoleBasedEmployeesService.java
+++
@@ -1,8 +0,0 @@
-package com.arshaa.emp.service;
-
-import org.springframework.http.ResponseEntity;
-
-public interface RoleBasedEmployeesService {
-	
-	public ResponseEntity getRoleBasedEmployeesByEmployeeId(String employeeId);
-}
