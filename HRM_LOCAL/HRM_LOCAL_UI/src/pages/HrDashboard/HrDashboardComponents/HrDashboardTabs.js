827196386e1cd6a3f4bcf885163c853790f5b67d
Murali_Krishna <muralikrishna25101997@gmail.com>
2022-11-02 19:18:08 +0530

    changes in leads and clients

--- HRM_LOCAL/HRM_LOCAL_UI/src/pages/HrDashboard/HrDashboardComponents/HrDashboardTabs.js
+++ HRM_LOCAL/HRM_LOCAL_UI/src/pages/HrDashboard/HrDashboardComponents/HrDashboardTabs.js
@@ -13,9 +13,6 @@ import EmployeeAttendenceMain from "../../EmployeeAttendancee/EmployeeAttendence
 import ClientandLeadTab from "./ClientandLeadTab";
 
 
-import ResignationTab from "./ResignationTab";
-import PMO_Dashboard from "../../Recruitment-Tracker/RequisitionDashboard/PMO_Dashboard/PMO_Dashboard";
-import HrProjectsTab from "./HrProjectsTab";
 
 
 const HrDashboardTabs = () => {
@@ -41,23 +38,21 @@ const HrDashboardTabs = () => {
                             <Box sx={{ width: '100%', typography: 'body1', paddingTop: "15px" }}>
                                 <TabContext value={value}>
                                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                                        <TabList onChange={handleChange} sx={{"& button.Mui-selected":{background: "#a3c2e3",color:"black"}}} aria-label="lab API tabs example"style={{background: "#354e69",borderRadius:"3px",height:"60px",color:"white"}}>
-                                            <Tab label="Hirings" value="1" style={{paddingRight:"2%",paddingLeft:"2%",fontSize:"14px"}} iconPosition="start" icon={<FcPositiveDynamic style={{fontSize:"20px"}} />}></Tab>
-                                            <Tab label="Employees" value="2" style={{paddingRight:"2%",paddingLeft:"2%",fontSize:"14px"}} iconPosition="start" icon={<FcConferenceCall style={{fontSize:"20px"}}/>} />
-                                            <Tab label="Attendance" value="3" style={{paddingRight:"2%",paddingLeft:"2%",fontSize:"14px"}} iconPosition="start" icon={<FcLeave style={{fontSize:"20px"}}/>}/>
-                                            <Tab label="Resignations" value="4" style={{paddingRight:"2%",paddingLeft:"2%",fontSize:"14px"}} iconPosition="start" icon={<FcNegativeDynamic style={{fontSize:"20px"}} />}></Tab>
-                                            <Tab label="Clients & Leads" value="5" style={{paddingRight:"2%",paddingLeft:"2%",fontSize:"14px"}} iconPosition="start" icon={<FcManager style={{fontSize:"20px"}}/>} />
-                                            <Tab label="Jobs" value="6" style={{paddingRight:"2%",paddingLeft:"2%",fontSize:"14px"}} iconPosition="start" icon={<FcCollaboration style={{fontSize:"20px"}}/>}/>
-                                            <Tab label="Projects" value="7" style={{paddingRight:"2%",paddingLeft:"2%",fontSize:"14px"}} iconPosition="start" icon={<FcNeutralTrading style={{fontSize:"20px"}}/>}/>                                            
+                                        <TabList onChange={handleChange} sx={{ "& button.Mui-selected": { background: "#a3c2e3", color: "black" } }} aria-label="lab API tabs example" style={{ background: "#354e69", borderRadius: "3px", height: "60px", color: "white" }}>
+                                            <Tab label="Hirings" value="1" style={{ paddingRight: "2%", paddingLeft: "2%", fontSize: "14px" }} iconPosition="start" icon={<FcPositiveDynamic style={{ fontSize: "20px" }} />}></Tab>
+                                            <Tab label="Employees" value="2" style={{ paddingRight: "2%", paddingLeft: "2%", fontSize: "14px" }} iconPosition="start" icon={<FcConferenceCall style={{ fontSize: "20px" }} />} />
+                                            <Tab label="Attendance" value="3" style={{ paddingRight: "2%", paddingLeft: "2%", fontSize: "14px" }} iconPosition="start" icon={<FcLeave style={{ fontSize: "20px" }} />} />
+                                            <Tab label="Resignations" value="4" style={{ paddingRight: "2%", paddingLeft: "2%", fontSize: "14px" }} iconPosition="start" icon={<FcPositiveDynamic style={{ fontSize: "20px" }} />}></Tab>
+                                            <Tab label="Clients & Leads" value="5" style={{ paddingRight: "2%", paddingLeft: "2%", fontSize: "14px" }} iconPosition="start" icon={<FcConferenceCall style={{ fontSize: "20px" }} />} />
+                                            <Tab label="Jobs" value="6" style={{ paddingRight: "2%", paddingLeft: "2%", fontSize: "14px" }} iconPosition="start" icon={<FcLeave style={{ fontSize: "20px" }} />} />
+                                            <Tab label="Projects" value="7" style={{ paddingRight: "2%", paddingLeft: "2%", fontSize: "14px" }} iconPosition="start" icon={<FcLeave style={{ fontSize: "20px" }} />} />
+
                                         </TabList>
                                     </Box>
-                                    <TabPanel style={{padding:"10px"}} value="1"><HiringsTab/></TabPanel>
-                                    <TabPanel style={{padding:"10px"}} value="2"><AllEmployees /></TabPanel>
-                                    <TabPanel style={{padding:"10px"}} value="3"><EmployeeAttendenceMain/></TabPanel>
-                                    <TabPanel style={{padding:"10px"}} value="4"><ResignationTab/> </TabPanel>
-                                    <TabPanel style={{padding:"10px"}} value="5"><ClientandLeadTab/> </TabPanel>
-                                    <TabPanel style={{padding:"10px"}} value="6"><PMO_Dashboard/> </TabPanel>
-                                    <TabPanel style={{padding:"10px"}} value="7"><HrProjectsTab/> </TabPanel>
+                                    <TabPanel style={{ padding: "10px" }} value="1"><HiringsTab /></TabPanel>
+                                    <TabPanel style={{ padding: "10px" }} value="2"><AllEmployees /></TabPanel>
+                                    <TabPanel style={{ padding: "10px" }} value="3"><EmployeeAttendenceMain /></TabPanel>
+                                    <TabPanel style={{ padding: "10px" }} value="5"><ClientandLeadTab /></TabPanel>
                                 </TabContext>
                             </Box>
                         </Card.Body>
