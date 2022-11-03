5e2676592f17aa55023feefa44331a624dcea338
MadhuriAllapureddy99 <madhuri.a9898@gmail.com>
2022-11-03 16:36:53 +0530

    Ageing done

--- HRM_LOCAL/HRM_LOCAL_JAVA/Recruitment-Tracker/src/main/java/com/recruitmenttracker/repository/RequisitionRequestRepository.java
+++ HRM_LOCAL/HRM_LOCAL_JAVA/Recruitment-Tracker/src/main/java/com/recruitmenttracker/repository/RequisitionRequestRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.recruitmenttracker.entity.RequisitionRequestEntity;
+import com.recruitmenttracker.modal.Ageing;
 @Repository
 public interface RequisitionRequestRepository extends JpaRepository<RequisitionRequestEntity, Long>{
 	
@@ -24,4 +26,12 @@ public interface RequisitionRequestRepository extends JpaRepository<RequisitionR
 	    RequisitionRequestEntity findByRequisitionId(String requisitionId);
 	    
 	   RequisitionRequestEntity getByRequisitionId(String requisitionId);
+	   
+	   @Query(value="select *, TIMESTAMPDIFF(day, request_initiated_date,now()) AS diffDaysCount from requisition_requests ", nativeQuery = true)
+	   List<RequisitionRequestEntity> getDataWithAgingDays();
+	   
+	   @Query(value=" SELECT DATEDIFF(now(),request_initiated_date) AS ageing from requisition_requests   where requisition_id=?1 ", nativeQuery=true)
+	      Integer  getDateDiff(@Param("requisition_id")  String requisitionId);
+	  
+	
 }
