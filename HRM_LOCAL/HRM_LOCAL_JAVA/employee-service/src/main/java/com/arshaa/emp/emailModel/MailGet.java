896b1f4eed3f5e4b8989ae1e1d285e43bf170e62
murali-miriyala <muralimiriyala34@gmail.com>
2022-11-17 13:51:53 +0530

    Onboarding flow is working

---
+++ HRM_LOCAL/HRM_LOCAL_JAVA/employee-service/src/main/java/com/arshaa/emp/emailModel/MailGet.java
@@ -0,0 +1,14 @@
+package com.arshaa.emp.emailModel;
+
+public class MailGet {
+	private String Email;
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+}
