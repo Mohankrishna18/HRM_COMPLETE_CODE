50480b9b183c8236a064b93bdbb479ae784ed2d7
murali-miriyala <muralimiriyala34@gmail.com>
2022-12-01 17:35:05 +0530

    Changes by miriyala

---
+++ HRM_LOCAL/HRM_LOCAL_JAVA/employee-service/src/main/java/com/arshaa/emp/model/RequsitionModel.java
@@ -0,0 +1,27 @@
+package com.arshaa.emp.model;
+
+public class RequsitionModel {
+	
+	private String departmentName;
+	private String jobTitle;
+	private String requisitionId;
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	public String getRequisitionId() {
+		return requisitionId;
+	}
+	public void setRequisitionId(String requisitionId) {
+		this.requisitionId = requisitionId;
+	}
+
+}
